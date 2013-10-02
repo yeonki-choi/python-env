87db7807646f47ed2514ef6d7240fcbf4e6d93f1
Sridhar Ratnakumar <sridharr@activestate.com>
2013-10-02 16:38:21 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from bottle import route, run, template
+
+html_template = r'''
+<title>Python Env</title>
+<body>
+<h1>Python Env</h1>
+<ul>
+  % for name, value in envlist.items():
+    <li><b>{{name}}</b>: <tt>{{value}}</tt></li>
+  % end
+</ul>
+</body>
+'''
+
+@route('/')
+def index():
+    return template(html_template, envlist=os.environ)
+
+run(host='0.0.0.0', port=int(os.getenv("PORT", 8080)))
